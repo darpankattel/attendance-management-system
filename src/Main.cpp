4a7dbe2c58bb00b6a46d3f014ec52cb4e20dd2af
Simran <simrandhakal2017@gmail.com>
2022-07-14 07:30:50 +0545

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,23 @@
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
+ 
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+#include <MyFrame.h>
+ 
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+ 
+
+wxIMPLEMENT_APP(MyApp);
+ 
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
